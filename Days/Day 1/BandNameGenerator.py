2747dd774485b1d7f27fab37e8df2061b973e598
Victor Ferrari <victorfonsecaferrari@gmail.com>
2022-04-13 10:01:18 -0300

    Create

---
+++ Days/Day 1/BandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Wellcome to the Band Name Genarator.")
+nameCity = input("What's name of the city you grew up? ")
+namePet = input("What's your pet's name? ")
+
+print("Your band name could be " + nameCity + " " + namePet)
